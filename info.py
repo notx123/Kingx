8dd790e64f44dd19d9f1b338a50d8d1a0871cbcf
Faznj <108731444+fazni7@users.noreply.github.com>
2024-01-17 16:09:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -49,7 +49,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Ajax_files')
 # Others
 IS_VERIFY = bool(environ.get('IS_VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'tnshort.net')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'b6fdd5223766ff346ba3b7c47f50b9cc9dc57458')
+SHORTLINK_API = environ.get('SHORTLINK_API', '3d163268589d63a849cf224c9a7783253024a8c5')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', True))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
