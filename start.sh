e1a12d30fd044ead7c2a678c16ea4820daf0ec96
notx123 <gitupmszx@gmail.com>
2024-07-28 11:24:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/notx123/Kingx.git /Kingx
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Kingx
 fi
-cd /DQ-The-File-Donor
+cd /Kingx
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting 😲...."
 python3 bot.py
